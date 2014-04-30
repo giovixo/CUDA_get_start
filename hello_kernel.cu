8239741365ebc3033f6a590deaa52b697eb8a837
Giovanni De Cesare <decesare.giovanni@gmail.com>
2014-04-30 15:26:39 +0200

    Added hello_kernel.cu

---
+++ hello_kernel.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * Questo è il programma più semplice che si può immaginare scritto con CUDA C
+ *
+ * Cosa fa? Di pratico nulla. Chiama però il kernel "dark", dimostrando che qualcosa
+ * di oscuro è accaduto sulla scheda grafica. Molte domande restano ancora in sospeso,
+ * ma il modello di programmazione host-kernel dovrebbe a questo punto essere chiaro.
+ *
+ */
+
+// Attenzione a questa parola chiave. Definisce un kernel, ovvero un processo che avviene
+// sulla GPU
+__global__ void dark(void)
+{
+	// Oggi non mi va di fare nulla. E in effetti non faccio niente.
+	// Nota che il printf non e' consen su un kernel
+	//printf("Hola!\n");
+}
+
+int main(void)
+{
+	// Lancio il kernel "dark"
+	dark<<<1,1>>>();
+	printf("Benvenuto nel magico mondo delle GPU. Guarda il file 'hello_kernel.cu' per capire di cosa si tratta.\n");
+	return 0;
+}
+
